ca3f0d653d39614030dec8cffdc823ed0dac6812
Asamaludi26 <a.samaludi@gmail.com>
2024-01-01 20:08:43 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import{Routes, Route} from 'react-router-dom'
 
 // components
 // import FaqComponents from './components/FaqComponents';
-import FooterComponents from './components/FooterComponents';
+// import FooterComponents from './components/FooterComponents';
 import NavbarComponent from './components/NavbarComponent';
 // pages
 import HomePage from './pages/HomePage'
@@ -21,13 +21,13 @@ function App() {
       <Routes>
         <Route path='/' Component={HomePage} />
         <Route path='/Kelas' Component={KelasPage} />
-        <Route path='/Testimoni' Component={TestiPage} />
+        <Route path='/Testimonial' Component={TestiPage} />
         <Route path='/FAQ' Component={FaqPage} />
-        <Route path='/S&K' Component={SnKPage} />
+        <Route path='/SNK' Component={SnKPage} />
         {/* <Route path='/' Component={KelasPage} /> */}
     </Routes>
 
-    <FooterComponents />
+    {/* <FooterComponents /> */}
     </div>
     
   );
--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,6 +1,64 @@
+import { useState, useEffect } from "react"
+
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import { navLinks } from "../data/index";
+import { NavLink } from "react-router-dom";
+
 const NavbarComponent = () => {
+  const[changeColor, setChangeColor] = useState(false);
+
+  const changeBackgroundColor = ()=> {
+    if (window.scrollY > 10){
+      setChangeColor(true);
+    }else{
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(()=>{
+    changeBackgroundColor();
+
+    window.addEventListener('scroll', changeBackgroundColor);
+  });
+
   return (
-    <div>NavbarComponent</div>
+    <div className="text-center">
+        <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
+          <Container>
+            <Navbar.Brand href="#home" className="fs-5 fw-bold">
+            KemayoranStreet</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                {navLinks.map((link)=>{
+                  return(
+                    <div className="nav-link" key={link.id}>
+                      <NavLink to={link.path}
+                      className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                      }end>{link.text}</NavLink>
+                    </div>
+                  );
+                })} 
+                
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {navLinks.map((link)=>{
+                  return(
+                    <div key={link.id}>
+                      <NavLink to={link.path}>{link.text}</NavLink>
+                    </div>
+                  );
+                })}
+                </NavDropdown>
+              </Nav>
+
+              <div className="text-center">
+                <button type="button" className="btn btn-outline-danger rounded-1">Join with us</button>
+              </div>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    </div>
   )
 }
 
